1d6832ad43159b62386b4ca917428f89ecf6b884
Ricardo Dias <rids@ipvc.pt>
2021-01-07 18:54:54 +0000

    Auto update Votes

--- server/endpoints/arcsRoute.js
+++ server/endpoints/arcsRoute.js
@@ -23,12 +23,12 @@ exports.createArcs = async function (req, res, contract) {
         }
         const key = uuidv4();
         const createdAt = new Date();
-        const { description, initialNode, finalNode, totalVotes } = req.body;
+        const { description, initialNode, finalNode } = req.body;
         await dataVerifications.verifyKeyExists(initialNode, 'Nodes', contract);
         await dataVerifications.verifyKeyExists(finalNode, 'Nodes', contract);
         await dataVerifications.verifyKeyExists(creatorId, 'Users', contract);
 
-        await contract.submitTransaction('createArcs', key, description, initialNode, finalNode, creatorId, createdAt, totalVotes);
+        await contract.submitTransaction('createArcs', key, description, initialNode, finalNode, creatorId, createdAt, 0);
         res.sendStatus(201);
     } catch (e) {
         res.status(500).json(e.message);
@@ -42,10 +42,27 @@ exports.updateArcs = async function (req, res, contract) {
             const userID = jwt.verify(req.body.token, "MySecret");
             creatorId = userID.userId;
         }
-        const { key, description, initialNode, finalNode, totalVotes } = req.body;
-        await dataVerifications.verifyKeyExists(initialNode, 'Nodes', contract);
-        await dataVerifications.verifyKeyExists(finalNode, 'Nodes', contract);
-        await dataVerifications.verifyKeyExists(creatorId, 'Users', contract);
+        const { key, description, initialNode, finalNode } = req.body;
+
+        if (initialNode !== "" && initialNode !== undefined) {
+            await dataVerifications.verifyKeyExists(initialNode, 'Nodes', contract);
+        }
+        if (finalNode !== "" && finalNode !== undefined) {
+            await dataVerifications.verifyKeyExists(finalNode, 'Nodes', contract);
+        }
+        if (creatorId !== "" && creatorId !== undefined) {
+            await dataVerifications.verifyKeyExists(creatorId, 'Users', contract);
+        }
+
+        //contagem do total votos do arco
+        const data = await contract.submitTransaction("queryByObjectType", "Votes");
+        let totalVotes = 0;
+        JSON.parse(data).forEach((votesData) => {
+            if (votesData.Record.arcId === key) {
+                totalVotes = totalVotes + Number(votesData.Record.vote);
+            }
+        });
+
         await contract.submitTransaction('updateArcs', key, description, initialNode, finalNode, creatorId, totalVotes);
         res.sendStatus(204);
     } catch (e) {
--- server/endpoints/votesRoute.js
+++ server/endpoints/votesRoute.js
@@ -30,10 +30,19 @@ exports.createVotes = async function (req, res, contract) {
         await dataVerifications.verifyKeyExists(voter, 'Users', contract);
         await dataVerifications.verifyKeyExists(arcId, 'Arcs', contract);
 
-        console.log('resultado: ' + voter + arcId + vote);
         const key = uuidv4();
         const createdAt = new Date();
         await contract.submitTransaction('createVotes', key, voter, arcId, vote, createdAt);
+
+        //atualizar quantidade votos nos arcos
+        const data = await contract.submitTransaction("queryByObjectType", "Votes");
+        let totalVotes = 0;
+        JSON.parse(data).forEach((votesData) => {
+            if (votesData.Record.arcId === arcId) {
+                totalVotes = totalVotes + Number(votesData.Record.vote);
+            }
+        });
+        await contract.submitTransaction('updateArcs', arcId, "", "", "", "", totalVotes);
         res.sendStatus(201);
     } catch (e) {
         res.status(500).json(e.message);
@@ -42,7 +51,23 @@ exports.createVotes = async function (req, res, contract) {
 
 exports.deleteVotes = async function (req, res, contract) {
     try {
+        //atualizar quantidade votos nos arcos
+        const data = await contract.submitTransaction("queryByObjectType", "Votes");
+        let arcId = '';
+        let totalVotes = 0;
+        JSON.parse(data).forEach((votesData) => {
+            if (votesData.Key === req.headers.key) {
+                arcId = votesData.Record.arcId;
+            }
+        });
+        JSON.parse(data).forEach((votesData) => {
+            if (votesData.Record.arcId === arcId && votesData.Key !== req.headers.key) {
+                totalVotes = totalVotes + Number(votesData.Record.vote);
+            }
+        });
+        
         await contract.submitTransaction('deleteVotes', req.headers.key);
+        await contract.submitTransaction('updateArcs', arcId, "", "", "", "", totalVotes);
         res.sendStatus(204);
     } catch (e) {
         res.status(500).json(e.message);
